58f184015e9f7a0c8c6fae97259a29e6103f2c0f
Graham Wright <gwright99@hotmail.com>
2024-03-31 07:12:27 -0400

    Can successfully call localstack pod from local machine

--- apps/testing/src/testing.py
+++ apps/testing/src/testing.py
@@ -8,9 +8,17 @@ os.environ["AWS_SECRET_ACCESS_KEY"]="456"
 
 # https://stackoverflow.com/questions/58828800/adding-custom-headers-to-all-boto3-requests
 # Add a custom header to every request (to work with localstack security)
+
+# some_client = boto3.client(service_name=SERVICE_NAME)
+# event_system = some_client.meta.events
+# event_system.register_first('before-sign.EVENT_NAME.*', _add_header)
+# You can try using a wildcard for all requests:
+# event_system.register_first('before-sign.*.*', _add_header)
+
 def _add_header(request, **kwargs):
     request.headers.add_header('header-secret', HEADERSECRET)
     print(request.headers)  # for debug
+
 
 def generate_aws_client(service: str, endpoint_url: str):
     client = boto3.client("s3", endpoint_url=endpoint_url)
@@ -19,18 +27,9 @@ def generate_aws_client(service: str, endpoint_url: str):
     return client
 
 
-# some_client = boto3.client(service_name=SERVICE_NAME)
-# event_system = some_client.meta.events
-# event_system.register_first('before-sign.EVENT_NAME.*', _add_header)
-# You can try using a wildcard for all requests:
-# event_system.register_first('before-sign.*.*', _add_header)
-    
+endpoint_url = "https://localstack.grahamwrightk8s.net"
 
-
-endpoint_url = "https://localstack.grahamwrightk8s.net" #:4566"
-
-# s3 = boto3.client("s3", endpoint_url=endpoint_url)
-# event_system = s3.meta.events
-# event_system.register_first('before-sign.*.*', _add_header)
-s3 = generate_aws_client(service="s3", endpoint_url=endpoint_url)
+s3 = boto3.client("s3", endpoint_url=endpoint_url)
+event_system = s3.meta.events
+event_system.register_first('before-sign.*.*', _add_header)
 print(s3.list_buckets())
